5fe660ce7058b3bb272eb5e2082c5db79e7c81ac
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-09-23 22:54:00 +0600

    social links added

--- src/components/SocialSection.tsx
+++ src/components/SocialSection.tsx
@@ -1,8 +1,14 @@
+import Link from "next/link";
+
 const SocialSection = () => {
   return (
     <div className="flex items-center">
       <div className="flex items-center gap-7">
-        <a href="#" className="text-black-50 hover:text-black-90">
+        <Link
+          href="https://www.facebook.com/designer.clan"
+          target="_blank"
+          className="text-black-50 hover:text-black-90"
+        >
           <span className="sr-only">Facebook</span>
           <svg
             fill="currentColor"
@@ -16,8 +22,12 @@ const SocialSection = () => {
               clip-rule="evenodd"
             ></path>
           </svg>
-        </a>
-        <a href="#" className="text-black-50 hover:text-black-90">
+        </Link>
+        <Link
+          href="https://www.instagram.com/designers.clan/"
+          target="_blank"
+          className="text-black-50 hover:text-black-90"
+        >
           <span className="sr-only">Instagram</span>
           <svg
             fill="currentColor"
@@ -31,8 +41,8 @@ const SocialSection = () => {
               clip-rule="evenodd"
             ></path>
           </svg>
-        </a>
-        <a href="#" className="text-black-50 hover:text-black-90">
+        </Link>
+        {/* <a href="#" className="text-black-50 hover:text-black-90">
           <span className="sr-only">Twitter</span>
           <svg
             fill="currentColor"
@@ -42,8 +52,26 @@ const SocialSection = () => {
           >
             <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"></path>
           </svg>
-        </a>
-        <a href="#" className="text-black-50 hover:text-black-90">
+        </a> */}
+        <Link
+          className="text-black-50 hover:text-black-90"
+          href="https://www.linkedin.com/company/designers-clan/"
+        >
+          <span className="sr-only">Linkedin</span>
+          <svg
+            viewBox="0 0 24 24"
+            className="h-5 w-5"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+          </svg>
+        </Link>
+        <Link
+          href="https://github.com/leadbangladesh"
+          target="_blank"
+          className="text-black-50 hover:text-black-90"
+        >
           <span className="sr-only">GitHub</span>
           <svg
             fill="currentColor"
@@ -57,7 +85,7 @@ const SocialSection = () => {
               clip-rule="evenodd"
             ></path>
           </svg>
-        </a>
+        </Link>
       </div>
     </div>
   );
