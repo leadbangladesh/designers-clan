2436236e2616f795a50f8ae84f9059d83eab5130
Jisan Mia <61211600+Jisan-mia@users.noreply.github.com>
2023-09-23 17:26:47 +0600

    prepared to deploy on vercel

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-
 const nextConfig = {
-  // basePath: isProd ? "/designers-clan" : "",
-  // output: "export",
-  distDir: "dist",
-  // images: {
-  //   unoptimized: true,
-  // },
 };
 
 module.exports = nextConfig
